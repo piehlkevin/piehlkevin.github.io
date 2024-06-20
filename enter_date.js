cf9df19ea64825b93db5736391eb70d361b6c8af
piehlkevin <piehl@wipol.uni-hannover.de>
2024-06-20 09:12:10 +0200

    Version as of 20/06/24

--- enter_date.js
+++ enter_date.js
@@ -1,3 +1,13 @@
+
+///window.onload = function() {
+///    document.getElementById('change_date').innerHTML = "20/06/2024"
+///}
+
 window.onload = function() {
-    document.getElementById('change_date').innerHTML = "20/06/2024"
+    const d = new Date();
+    const year = d.getFullYear();
+    const month = d.getMonth();
+    const day = d.getDay();
+    const current_date = day.toString() + "/" + month.toString() + "/" + year.toString();
+    document.getElementById('change_date').innerHTML = current_date;
 }
