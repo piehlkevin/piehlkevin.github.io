3e6bb7ea01c90fcbe99af4d12126f8b88b32b20d
piehlkevin <piehl@wipol.uni-hannover.de>
2024-06-20 09:22:12 +0200

    Version as of 20/06/24

--- enter_date.js
+++ enter_date.js
@@ -1,18 +1,6 @@
-
-///window.onload = function() {
-///    document.getElementById('change_date').innerHTML = "20/06/2024"
-///}
-
+/// Adjust the date of the last update on all pages:
 window.onload = function() {
-    const d = new Date();
-    console.log(d);
-    const year = d.getFullYear();
-    console.log("year: ", year);
-    const month = d.getMonth();
-    console.log("month: ", month);
-    const day = d.getDay();
-    console.log("day: ", day);
-    const current_date = day.toString() + "/" + month.toString() + "/" + year.toString();
-    console.log("current date: ", current_date);
-    document.getElementById('change_date').innerHTML = current_date;
+    document.getElementById('change_date').innerHTML = "20/06/2024"
 }
+
+/// TODO: Transfer all other functions into this file as well and rename the file
