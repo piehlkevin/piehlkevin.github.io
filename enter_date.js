580f186e457eb92ef610286dca5ff7ba2aa2ac38
piehlkevin <piehl@wipol.uni-hannover.de>
2024-06-20 09:18:21 +0200

    Version as of 20/06/24

--- enter_date.js
+++ enter_date.js
@@ -7,8 +7,12 @@ window.onload = function() {
     const d = new Date();
     console.log(d);
     const year = d.getFullYear();
+    console.log("year: ", year);
     const month = d.getMonth();
+    console.log("month: ", month);
     const day = d.getDay();
+    console.log("day: ", day);
     const current_date = day.toString() + "/" + month.toString() + "/" + year.toString();
+    console.log("current date: ", current_date);
     document.getElementById('change_date').innerHTML = current_date;
 }
