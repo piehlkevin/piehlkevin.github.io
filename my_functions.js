76bacef69182cd792dbc45e1f6614a732e21b15b
piehlkevin <piehl@wipol.uni-hannover.de>
2024-07-13 10:38:53 +0200

    Version as of 13/07/24

--- my_functions.js
+++ my_functions.js
@@ -1,6 +1,6 @@
 /// Adjust the date of the last update on all pages:
 function PasteChangeDate() {
-    document.getElementById('change_date').innerHTML = "02/07/2024";
+    document.getElementById('change_date').innerHTML = "13/07/2024";
 }
 
 /// Open a new window to write an e-mail:
@@ -17,82 +17,162 @@ function MakeTextBlue(text) {
     text.style.color = '#4996bf';
 }
 
+function MakeTextWhite(text) {
+    text.style.color = '#ffffff';
+}
+
 /// Create the animations for the boxes:
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            if (entry.target.getAttribute('class') === 'info_box_black_more' || entry.target.getAttribute('class') === 'info_box_black_less'){
+            // The first page is a bit special. So do this one first:
+            if (entry.target.getAttribute('id') === 'photo_1' ||
+                entry.target.getAttribute('id') === 'photo_3') {
+                entry.target.classList.add('dates_enter')
+            }
+            else if (entry.target.getAttribute('id') === 'photo_2' ||
+                entry.target.getAttribute('id') === 'photo_4') {
+                entry.target.classList.add('dates_enter_alt')
+            }
+            else if (entry.target.getAttribute('id') === 'box_1' ||
+                entry.target.getAttribute('id') === 'box_2' ||
+                entry.target.getAttribute('id') === 'box_3' ||
+                entry.target.getAttribute('id') === 'box_4') {
                 entry.target.classList.add('info_box_enter')
             }
-            if (entry.target.getAttribute('class') === 'date'){
-                entry.target.classList.add('dates_enter')
+            // Now, continue with the "normal" ones:
+            else {
+                if (entry.target.getAttribute('class') === 'info_box_black_more' || entry.target.getAttribute('class') === 'info_box_black_less') {
+                    entry.target.classList.add('info_box_enter')
+                }
+                if (entry.target.getAttribute('class') === 'date' || entry.target.getAttribute('class') === 'logo') {
+                    entry.target.classList.add('dates_enter')
+                }
             }
         }
     })
 });
 
 const my_boxes = [
-    // Welcome: (none)
+    // Welcome:
+    'box_1',
+    'photo_1',
+    'box_2',
+    'photo_2',
+    'box_3',
+    'photo_3',
+    'box_4',
+    'photo_4',
     // Academic formation:
     'box_phd_program',
     'box_master_degree',
     'date_master_degree',
+    'logo_master_degree',
     'box_master_program',
     'date_master_program',
+    'logo_master_program',
     'box_bachelor_degree',
     'date_bachelor_degree',
+    'logo_bachelor_degree',
     'box_bachelor_program',
     'date_bachelor_program',
+    'logo_bachelor_program',
     'box_abitur_degree',
     'date_abitur_degree',
+    'logo_abitur_degree',
     'box_abitur_program',
     'date_abitur_program',
+    'logo_abitur_program',
     'box_grundschule_program',
     'date_grundschule_program',
+    'logo_grundschule_program',
     // Career path:
     'box_student_asistant_MAK',
     'date_student_assistant_MAK',
+    'logo_student_assistant_MAK',
     'box_student_asistant_HSP',
     'date_student_assistant_HSP',
+    'logo_student_assistant_HSP',
     'box_FSJ',
     'date_FSJ',
+    'logo_FSJ',
     'box_stadt_events',
     'date_stadt_events',
+    'logo_stadt_events',
     'box_IAV',
     'date_IAV',
+    'logo_IAV',
     // Grants & Awards:
     'box_master',
     'date_master',
+    'logo_master',
     'box_deanslist_1',
     'date_deanslist_1',
+    'logo_deanslist_1',
     'box_deanslist_2',
     'date_deanslist_2',
+    'logo_deanslist_2',
     'box_deanslist_3',
     'date_deanslist_3',
+    'logo_deanslist_3',
     'box_deanslist_4',
     'date_deanslist_4',
+    'logo_deanslist_4',
     'box_bachelor',
     'date_bachelor',
+    'logo_bachelor',
     // Research:
     'box_project_1',
     'box_project_2',
     'box_project_3',
     // Teaching:
     'winter_2324',
+    'logo_winter_2324',
     'exercise_winter_2324',
     'summer_2023',
+    'logo_summer_2023',
     'exercise_summer_2023',
     'winter_2223',
+    'logo_winter_2223',
     'exercise_winter_2223',
     'seminar_winter_2223',
     'summer_2022',
+    'logo_summer_2022',
     'exercise_summer_2022',
     'seminar_summer_2022',
     'winter_2122',
+    'logo_winter_2122',
     'exercise_winter_2122',
     'seminar1_winter_2122',
     'seminar2_winter_2122',
-    // Engagement: (none)
+    // Engagement:
+    'date_FM',
+    'logo_FM',
+    'box_FM',
+    'date_XXX',
+    'logo_XXX',
+    'box_XXX',
+    'date_klettern',
+    'logo_klettern',
+    'box_klettern',
+    'date_kifebe',
+    'logo_kifebe',
+    'box_kifebe',
+    'date_DHM',
+    'logo_DHM',
+    'box_DHM',
+    'date_X',
+    'logo_X',
+    'box_X',
+    'date_KFV',
+    'logo_KFV',
+    'box_KFV',
+    'date_Jugendwart',
+    'logo_Jugendwart',
+    'box_Jugendwart',
+    'date_Trainer',
+    'logo_Trainer',
+    'box_Trainer',
     // Misc: (none)
 ]
 
